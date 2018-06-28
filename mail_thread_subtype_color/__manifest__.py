628711f33764f2bf972add33253acec81bcbd3ba
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-06-28 15:44:13 +0300

    Fix asset inherit

--- mail_thread_subtype_color/__manifest__.py
+++ mail_thread_subtype_color/__manifest__.py
@@ -23,7 +23,7 @@
     'name': 'Colors for mail thread subtypes',
     'summary': 'Add colors for mail thread subtypes',
     'category': 'Sale',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
