04a6e62670fef55111182671a77bb9e14046a5bd
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2022-02-25 12:08:14 +0200

    Created a new module

---
+++ mass_mailing_subject_not_translatable/models/mailing.py
@@ -0,0 +1,7 @@
+from odoo import fields, models
+
+
+class MassMailing(models.Model):
+    _inherit = "mailing.mailing"
+
+    subject = fields.Char('Subject', help='Subject of your Mailing', required=True, translate=False)
