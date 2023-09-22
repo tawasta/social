11ddecde854b9877d839e31397d62b61133db8a6
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-09-22 11:53:23 +0300

    Modified module to 14 version

---
+++ mail_chatter_colors/__manifest__.py
@@ -0,0 +1,36 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2020 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Chatter colors",
+    "summary": "Chatter colors",
+    "version": "14.0.1.0.0",
+    "category": "Social",
+    "website": "https://tawasta.fi",
+    "author": "Tawasta",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "mail",
+    ],
+    "data": ["views/assets.xml"],
+    "qweb": ["static/src/xml/chatter.xml"],
+}
