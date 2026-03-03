ccd853bb6798894bd7c40641c75862f3bb2ccd1a
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-03-03 08:48:20 +0200

    Fix JSON parsing of webhook controller

--- mail_tracking_postmark/controllers/main.py
+++ mail_tracking_postmark/controllers/main.py
@@ -3,7 +3,7 @@ import logging
 from odoo.http import request, route
 
 from ...mail_tracking.controllers import main
-from ...web.controllers.main import ensure_db
+from odoo.addons.web.controllers.utils import ensure_db
 
 _logger = logging.getLogger(__name__)
 
@@ -16,6 +16,6 @@ class MailTrackingController(main.MailTrackingController):
 
         # Process event
         request.env["mail.tracking.email"].sudo()._postmark_event_process(
-            request.jsonrequest,
+            request.get_json_data(),
             self._request_metadata(),
         )
