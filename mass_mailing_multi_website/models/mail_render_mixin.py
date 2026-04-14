864da5962152769681e39bc0a21b2fb02aaa16c8
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-04-14 12:36:07 +0300

    Use mailing.mailing.get_base_url if it is the active model

--- mass_mailing_multi_website/models/mail_render_mixin.py
+++ mass_mailing_multi_website/models/mail_render_mixin.py
@@ -8,6 +8,20 @@ class MailRenderMixin(models.AbstractModel):
     _inherit = "mail.render.mixin"
 
     def _render_template_postprocess(self, rendered):
-        if self.env.context.get("active_model") == "mailing.mailing":
-            self = self.with_context(mail_render_postprocess_model="mailing.mailing")
-        return super(MailRenderMixin, self)._render_template_postprocess(rendered)
+        model = self.env.context.get('mail_render_postprocess_model')
+        active_model = self.env.context.get('active_model')
+        res_ids = list(rendered.keys())
+        mailing_mailing = None
+        active_model = self.env.context.get("active_model")
+        active_id = self.env.context.get("active_id")
+        if active_model == "mailing.mailing" and active_id:
+            mailing_mailing = self.env["mailing.mailing"].search([('id', '=', active_id)], limit=1)
+        for res_id, rendered_html in rendered.items():
+            base_url = None
+            if mailing_mailing:
+                base_url = mailing_mailing.get_base_url()
+            elif model:
+                base_url = self.env[model].browse(res_id).with_prefetch(res_ids).get_base_url()
+            rendered[res_id] = self._replace_local_links(rendered_html, base_url)
+        return rendered
+
