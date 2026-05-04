bfb10cb6aaf574a7da36ae6e9c47a10ab6ee0750
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-05-04 13:59:58 +0300

    Add debug logging

--- mass_mailing_multi_website/controllers/main.py
+++ mass_mailing_multi_website/controllers/main.py
@@ -13,9 +13,15 @@ class Website(Website):
     def get_suggested_link(self, needle, limit=10):
         current_website = request.website
         result = super().get_suggested_link(needle, limit)
+        _logger.error("HERE: current_website ")
+        _logger.error(current_website)
+        _logger.error("HERE: result ")
+        _logger.error(result)
         # Add the website url to the beginning of urls so later it can not be set wrong
         if "matching_pages" in result:
             for item in result["matching_pages"]:
+                _logger.error("HERE: pages item ")
+                _logger.error(item)
                 if "domain" in current_website:
                     item["value"] = current_website["domain"] + item["value"]
                     if "label" in item and item["label"].startswith("/"):
@@ -23,6 +29,8 @@ class Website(Website):
         if "others" in result:
             for pages in result["others"]:
                 for item in pages["values"]:
+                    _logger.error("HERE: others item ")
+                    _logger.error(item)
                     if "domain" in current_website:
                         item["value"] = current_website["domain"] + item["value"]
                         if "icon" in item and item["icon"].startswith("/"):
