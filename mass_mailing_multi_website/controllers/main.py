82432b68731a9718ee09f5e77a3e0ae05fc41d71
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-05-04 13:31:38 +0300

    Add debug logging

--- mass_mailing_multi_website/controllers/main.py
+++ mass_mailing_multi_website/controllers/main.py
@@ -13,12 +13,8 @@ class Website(Website):
     def get_suggested_link(self, needle, limit=10):
         current_website = request.website
         result = super().get_suggested_link(needle, limit)
-        _logger.error("HERE: current_website ")
-        _logger.error(current_website)
         # Add the website url to the beginning of urls so later it can not be set wrong
         if "matching_pages" in result:
-            _logger.error("HERE: result ")
-            _logger.error(result)
             for item in result["matching_pages"]:
                 if "domain" in current_website:
                     item["value"] = current_website["domain"] + item["value"]
@@ -29,8 +25,8 @@ class Website(Website):
                 for item in pages["values"]:
                     if "domain" in current_website:
                         item["value"] = current_website["domain"] + item["value"]
-                    if "icon" in item and item["icon"].startswith("/"):
-                        item["icon"] = current_website["domain"] + item["icon"]
-                    if "label" in item and item["label"].startswith("/"):
-                        item["label"] = current_website["domain"] + item["label"]
+                        if "icon" in item and item["icon"].startswith("/"):
+                            item["icon"] = current_website["domain"] + item["icon"]
+                        if "label" in item and item["label"].startswith("/"):
+                            item["label"] = current_website["domain"] + item["label"]
         return result
