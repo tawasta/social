3c12a7e89cc3361a700fdbc54f21262ee4e0c90e
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-06-02 11:58:04 +0300

    Remove debug logging. Pre-commit fixes

--- mass_mailing_multi_website/controllers/main.py
+++ mass_mailing_multi_website/controllers/main.py
@@ -13,21 +13,25 @@ class Website(Website):
     def get_suggested_link(self, needle, limit=10):
         current_website = request.website
         result = super().get_suggested_link(needle, limit)
-        _logger.error("HERE: current_website ")
-        _logger.error(current_website)
-        _logger.error("HERE: result ")
-        _logger.error(result)
         # Add the website url to the beginning of urls so later it can not be set wrong
         if "matching_pages" in result:
             for item in result["matching_pages"]:
-                if "domain" in current_website and item["value"] and current_website["domain"]:
+                if (
+                    "domain" in current_website
+                    and item["value"]
+                    and current_website["domain"]
+                ):
                     item["value"] = current_website["domain"] + item["value"]
                     if "label" in item and item["label"].startswith("/"):
                         item["label"] = current_website["domain"] + item["label"]
         if "others" in result:
             for pages in result["others"]:
                 for item in pages["values"]:
-                    if "domain" in current_website and item["value"] and current_website["domain"]:
+                    if (
+                        "domain" in current_website
+                        and item["value"]
+                        and current_website["domain"]
+                    ):
                         item["value"] = current_website["domain"] + item["value"]
                         if "icon" in item and item["icon"].startswith("/"):
                             item["icon"] = current_website["domain"] + item["icon"]
